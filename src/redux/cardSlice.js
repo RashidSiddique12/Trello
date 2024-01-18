f50743fd5819dcd302562ce6d6fe380732f479b0
Rashid <stylorashid786@gmail.com>
2024-01-18 12:47:06 +0530

    minor change

--- src/redux/cardSlice.js
+++ src/redux/cardSlice.js
@@ -9,7 +9,6 @@ export const cardSlice = createSlice({
   initialState,
   reducers: {
     displayCards: (state, action) => {
-      // Assuming action.payload is an array of cards
       console.log("red", action.payload)
       const uniqueNewCards = action.payload.filter(
         (newCard) => !state.cards.some((existingCard) => existingCard.id === newCard.id)
@@ -36,39 +35,3 @@ export const cardSlice = createSlice({
 export const { displayCards, createNewCard, deleteCard } = cardSlice.actions;
 
 export default cardSlice.reducer;
-
-
-// export const cardSlice = createSlice({
-//   name: "cards",
-//   initialState,
-//   reducers: {
-//     displayCards: (state, action) => {
-//       //   console.log(action.payload.data);
-//       state.cards = {
-//         ...state.cards,
-//         [action.payload.id]: action.payload.data,
-//       };
-//     },
-
-  
-//     createNewCard: (state, action) => {
-//       console.log(state.cards[action.payload.id]);
-//       console.log(action.payload.data);
-//       state.cards = {
-//         ...state.cards,
-//         [action.payload.id]: [
-//           ...state.cards[action.payload.id],
-//           action.payload.data,
-//         ],
-//       };
-//     },
-//     deleteCard: (state, action) =>  {
-//       const temp = state.cards[action.payload.listId].filter(
-//         (card) => {
-//           return card.id !== action.payload.cardId;
-//         }
-//       );
-//       state.cards[action.payload.listId] = temp;
-//     },
-//   },
-// });
